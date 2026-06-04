4af707e5741681b6af84fc69f66b25260b5a4645
shailjabmishra <shailjabhartimishra1999@gmail.com>
2026-06-04 22:15:04 +0530

    main function

--- space_explorer.py
+++ space_explorer.py
@@ -5,52 +5,6 @@ import yaml
 import urllib.request
 import psycopg2
 
-
-
-def read_config():
-    with open("config.yaml", "r") as f:
-        config = yaml.safe_load(f)
-        return config
-CONFIG = read_config()
-def main():
-    while True:
-        print("What would you like to do?")
-
-        print("[1]  Explore a planet")
-        print("[2]  Get NASA Astronomy Picture of the Day")
-        print("[3]  List all planets")
-        print("[q]  Quit")
-        user_input = input("Enter your choice: ")
-        if user_input == "1":
-            print("Exploring a planet...")
-            which_planet = input("Which planet would you like to explore? ")
-            for planet in CONFIG['planets']:
-                if planet["name"].lower() == which_planet.lower():
-                    print(f"Name: {planet['name']}")
-                    print(f"Distance from Sun: {planet['distance_from_sun_km']} million km")
-                    print(f"Number of moons: {planet['moons']}")
-                    print(f"Fun fact: {planet['fun_fact']}")
-        elif user_input == "2":
-            print("Getting NASA Astronomy Picture of the Day...")
-            try:
-                with urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY") as response:
-                    data = response.read().decode("utf-8")  # bytes → string
-                    data_dict = json.loads(data)
-                    print(f"Title: {data_dict['title']}")
-                    print(f"date: {data_dict['date']}")
-                    print(f"Explanation: {data_dict['explanation'][:300]}")
-            except Exception as e:
-                print(f"Error occurred while fetching NASA APOD: {e}")
-        elif user_input == "3":
-            print("Listing all planets...")
-            for planet in CONFIG['planets']:
-                print(planet["name"])
-        elif user_input == "q":
-            print("Quitting...")
-            exit(0)
-        else:
-            print("Unknown option. Please type 1, 2, 3, or q.")
-
 def get_connection(db_config:dict):
     try:
         connection = psycopg2.connect(
@@ -182,4 +136,85 @@ def get_apod_history(conn) -> list:
         ]
     return apod_list
 
-main()
+def main():
+    def read_config():
+        with open("config.yaml", "r") as f:
+            config = yaml.safe_load(f)
+            return config
+    CONFIG = read_config()
+    db_config = CONFIG['Database']
+    planets = CONFIG['planets']
+    conn = None
+    try:
+        conn = get_connection(db_config)
+        create_table(conn)
+        seed_planets_from_config(conn,planets)
+        save_apod(conn,apod_data)
+
+        while True:
+            print("What would you like to do?")
+
+            print("[1]  Explore a planet")
+            print("[2]  Get NASA Astronomy Picture of the Day")
+            print("[3]  List all planets")
+            print("[4]  Mark a planet as visited")
+            print("[5]  Delete a planet")
+            print("[6]  View APOD history")
+            print("[q]  Quit")
+            user_input = input("Enter your choice: ")
+            if user_input == "1":
+                print("Exploring a planet...")
+                which_planet = input("Which planet would you like to explore? ")
+                for planet in CONFIG['planets']:
+                    if planet["name"].lower() == which_planet.lower():
+                        print(f"Name: {planet['name']}")
+                        print(f"Distance from Sun: {planet['distance_from_sun_km']} million km")
+                        print(f"Number of moons: {planet['moons']}")
+                        print(f"Fun fact: {planet['fun_fact']}")
+            elif user_input == "2":
+                print("Getting NASA Astronomy Picture of the Day...")
+                try:
+                    with urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY") as response:
+                        data = response.read().decode("utf-8")  # bytes → string
+                        data_dict = json.loads(data)
+                        print(f"Title: {data_dict['title']}")
+                        print(f"date: {data_dict['date']}")
+                        print(f"Explanation: {data_dict['explanation'][:300]}")
+                except Exception as e:
+                    print(f"Error occurred while fetching NASA APOD: {e}")
+            elif user_input == "3":
+                print("Listing all planets...")
+                get_all_planets(conn)
+                for planets in planets:
+                    print(planets)
+            
+            elif user_input == '4':
+                planet_name = input("Enter the planet you want to visit")
+                print("Visiting the planet")
+                mark_planet_visited(conn,planet_name)
+            
+            elif user_input=='5':
+                planet_name = input("Enter the planet you want the date to be deleted")
+                print("Deleting the planet")
+                delete_planet(conn,planet_name)
+
+            elif user_input == '6':
+                apod_data = get_apod_history(conn)
+                for data in apod_data:
+                    print(data)
+
+            elif user_input == "q":
+                print("Quitting...")
+                exit(0)
+            else:
+                print("Unknown option. Please type 1, 2, 3, or q.")
+
+    finally:
+        if conn:
+            conn.close()
+
+if __name__ == "__main__":
+    main()
+
+
+   
